f3e4ea2d86aca5443fdc2ce3e9a1b9b6ecd28e57
akshayswami17 <akshayswami1731@gmail.com>
2024-04-22 15:11:21 +0530

    Some modifications.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Todotable from './components/Todotable';
+import Tabledata from './components/Tabledata';
 
 function App() {
   return (
     <>
     <center >
     <div ><h1 className='to-do-container'>To Do List</h1></div>
-    <div class="container text-center">
-  
-  <div class="row">
-    <div class="col-6">
-    <input type="text" id="textInput" placeholder="Type something..."/></div>
-    <div class="col-4"><input type="date" id="dateInput" /></div>
-    <div class="col-2"><button type="button" class="btn btn-success">Add</button></div>
-  </div>
-</div>
+    <Todotable></Todotable>
+    <Tabledata></Tabledata>
     
     </center>
 
