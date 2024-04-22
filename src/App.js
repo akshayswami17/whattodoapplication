e80e8637a358b272a0f28e9f706a650501b062d1
akshayswami17 <akshayswami1731@gmail.com>
2024-04-22 12:01:19 +0530

    Added new repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <center >
+    <div ><h1 className='to-do-container'>To Do List</h1></div>
+    <div class="container text-center">
+  
+  <div class="row">
+    <div class="col-6">
+    <input type="text" id="textInput" placeholder="Type something..."/></div>
+    <div class="col-4"><input type="date" id="dateInput" /></div>
+    <div class="col-2"><button type="button" class="btn btn-success">Add</button></div>
+  </div>
+</div>
+    
+    </center>
+
+    
+    </>
   );
 }
 
